7e531ddfc4deb213f1a6866a18968c57de48fcdd
Skymonster97 <Skymonster97@gmail.com>
2019-05-24 08:20:24 +0600

    refactor(TypeResolver): update 'relevantMessage' type

--- src/struct/commands/arguments/TypeResolver.js
+++ src/struct/commands/arguments/TypeResolver.js
@@ -326,10 +326,9 @@ class TypeResolver {
 
             [ArgumentTypes.RELEVANT_MESSAGE]: async (message, phrase) => {
                 if (!phrase) return null;
+
                 const hereMsg = await message.channel.messages.fetch(phrase).catch(() => null);
-                if (hereMsg) {
-                    return hereMsg;
-                }
+                if (hereMsg) return hereMsg;
 
                 if (message.guild) {
                     for (const channel of message.guild.channels.cache.values()) {
@@ -337,7 +336,7 @@ class TypeResolver {
                         try {
                             return await channel.messages.fetch(phrase);
                         } catch (err) {
-                            if (/^Invalid Form Body/.test(err.message)) return null;
+                            if (/^Invalid Form Body/.test(err.message)) break;
                         }
                     }
                 }
